9aeae308d8034b3f1bb09fa4438a3866a3bc74c2
Andrew Walker <andrew.walker@earth.ox.ac.uk>
2025-05-14 08:46:16 +0100

    Add recheck option

--- path_check.py
+++ path_check.py
@@ -22,6 +22,7 @@ def check_file_pair(file, root, dest_base, source_base, verbose):
     else:
         print(f"MISSING: {dest_file}")
 
+
 def walk_source(source, dest, verbose=True):
     """
     Recursively look at all files in source and
@@ -44,6 +45,34 @@ def walk_source(source, dest, verbose=True):
                 print(f"---> exception type: {type(ex)}")
                 print(ex)
 
+
+def recheck(output_file, source, dest, verbose=True):
+    """
+    Check files in source match files in dest but only for those marked ERROR in output_file
+    
+    This is for rechecking after long running jobs where the remote file server sometimes
+    falls over
+    """
+    source_base = pathlib.Path(source)
+    assert source_base.is_dir(), "Source argument must be a directory"
+
+    dest_base = pathlib.Path(dest)
+    assert dest_base.is_dir(), "Dest argument must be a directory"
+
+    with open(output_file, "r") as f:
+        for line in f:
+            if line[0:3] == 'IN:':
+                current_root = line.split(':')[1]
+            elif line[0:6] == 'ERROR:':
+                check_file = line.split(':')[1]
+                try:
+                    check_file_pair(check_file, current_root, dest_base, source_base, verbose)
+                except Exception as ex:
+                    print(f"ERROR: {root/file}")
+                    print(f"---> exception type: {type(ex)}")
+                    print(ex)
+
+
 if __name__ == "__main__":
     import argparse
 
@@ -51,5 +80,10 @@ if __name__ == "__main__":
     parser.add_argument("source")
     parser.add_argument("dest")
     parser.add_argument('--verbose', '-v', action='store_true')
+    parser.add_argument('--recheck_errors', action='store',
+                        help='Provide output file from previous run, only files marked ERROR are checked.')
     args = parser.parse_args()
-    walk_source(args.source, args.dest, args.verbose)
+    if args.recheck_errors is None:
+        walk_source(args.source, args.dest, args.verbose)
+    else:
+        recheck(args.recheck_errors, args.source, args.dest, args.verbose)
